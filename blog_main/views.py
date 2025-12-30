17f14a0c445fc8498e288e6118b732a7c306fcab
RahmatUllahSA <rahmatullah.iubat.cse@gmail.com>
2025-12-30 23:06:45 +0600

    category show in the home page when new category add in admin panel

--- blog_main/views.py
+++ blog_main/views.py
@@ -1,6 +1,11 @@
-from django.http import HttpResponse
+#from django.http import HttpResponse
 from django.shortcuts import render
+from blogs.models import Category
 
 def home(request):
     #return HttpResponse("Welcome to the Blog Home Page!")
-    return render(request, 'home.html')
+    categories = Category.objects.all()
+    context = {
+        'categories': categories
+    }
+    return render(request, 'home.html', context)
